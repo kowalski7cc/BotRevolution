7357e784236b2e129d2fb60f7ba0fc05f41152ff
Kowalski Prime <kowalski.7cc@xspacesoft.com>
2019-03-19 14:33:56 +0100

    Bugfix for telegram method

--- src/main/java/com/kowalski7cc/botrevolution/actions/GetChatMember.java
+++ src/main/java/com/kowalski7cc/botrevolution/actions/GetChatMember.java
@@ -1,5 +1,7 @@
 package com.kowalski7cc.botrevolution.actions;
 
+import com.kowalski7cc.botrevolution.types.Message;
+import com.kowalski7cc.botrevolution.types.User;
 import com.kowalski7cc.botrevolution.types.chat.Chat;
 import com.kowalski7cc.botrevolution.types.chat.ChatMember;
 import com.kowalski7cc.botrevolution.utils.BotMethod;
@@ -15,6 +17,7 @@ import java.util.Optional;
 public class GetChatMember extends MethodBuilder<ChatMember> {
 
     private String chatID;
+    private Integer userID;
 
     public GetChatMember(String token, Integer timeout) {
         super(token, timeout);
@@ -35,10 +38,27 @@ public class GetChatMember extends MethodBuilder<ChatMember> {
         return this;
     }
 
+    public GetChatMember setUserID(Integer userID) {
+        this.userID = Objects.requireNonNull(userID);
+        return this;
+    }
+
+    public GetChatMember setUserID(User user) {
+        this.userID = Objects.requireNonNull(user.getId());
+        return this;
+    }
+
+    public GetChatMember setFromMessage(Message message) {
+        this.userID = message.getFrom().get().getId();
+        this.chatID = message.getChat().getId().toString();
+        return this;
+    }
+
     @Override
     public Optional<ChatMember> send() {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("chat_id", Objects.requireNonNull(chatID));
+        parameters.put("user_id", Objects.requireNonNull(userID).toString());
         return RequestHelper.get(token, BotMethod.GETCHATMEMBER, parameters, timeout)
                 .map(object -> ChatMemberDecoder.decode(ResponseDecoder.decode(object)));
     }
